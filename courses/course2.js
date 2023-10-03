898d1392f64ea1fe86e7eda01232c0e6f3a0ba46
Valentin <125616554+dobrowwwww@users.noreply.github.com>
2023-10-04 01:50:06 +0300

    /

--- courses/course2.js
+++ courses/course2.js
@@ -1,21 +1,14 @@
-// courses/course1.js
 var course2 = {
   name: "Курс 1",
+  subtitle: "Подзаголовок Курса 1",
   subcourses: [
     {
       name: "Подкурс 1",
+      subtitle: "Подзаголовок Подкурса 1",
       lessons: [
-        {
-          name: "Урок 1231231231",
-          videoId: "VIDEO_ID1",
-          assignments: [
-            { name: "Задание 1", url: "https://www.example.com/assignment1" },
-            // ... другие задания ...
-          ],
-        },
-        // ... другие уроки ...
+        // ...
       ],
     },
-    // ... другие подкурсы ...
+    // ...
   ],
 };
--- scripts.js
+++ scripts.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     el: "#app",
     data: {
       currentPage: "home",
-      courses: [annansuomicourse, IvanPetrichenkoWEB],
+      courses: [annansuomicourse, IvanPetrichenkoWEB, course2],
       currentCourse: null,
       currentSubcourse: null,
       currentLesson: null,
+      currentTask: null,
     },
     methods: {
       openCourse(course) {
@@ -21,6 +22,10 @@ document.addEventListener("DOMContentLoaded", function () {
         this.currentLesson = lesson;
         this.currentPage = "lesson";
       },
+      opentask(task) {
+        this.currentTask = task;
+        this.currentPage = "task";
+      },
       goBack() {
         if (this.currentPage === "course") {
           this.currentPage = "home";
@@ -28,10 +33,43 @@ document.addEventListener("DOMContentLoaded", function () {
           this.currentPage = "course";
         } else if (this.currentPage === "lesson") {
           this.currentPage = "subcourse";
+        } else if (this.currentPage === "task") {
+          this.currentPage = "lesson";
         }
+
       },
       getYoutubeEmbedUrl(videoId) {
         return `https://www.youtube.com/embed/${videoId}`;
+      },
+      getTotalLessons(course) {
+        return course.subcourses.reduce(
+          (total, subcourse) => total + subcourse.lessons.length,
+          0
+        );
+      },
+    },
+    computed: {
+      previousLesson() {
+        if (this.currentPage === "lesson" && this.currentSubcourse) {
+          const index = this.currentSubcourse.lessons.indexOf(
+            this.currentLesson
+          );
+          if (index > 0) {
+            return this.currentSubcourse.lessons[index - 1];
+          }
+        }
+        return null;
+      },
+      nextLesson() {
+        if (this.currentPage === "lesson" && this.currentSubcourse) {
+          const index = this.currentSubcourse.lessons.indexOf(
+            this.currentLesson
+          );
+          if (index < this.currentSubcourse.lessons.length - 1) {
+            return this.currentSubcourse.lessons[index + 1];
+          }
+        }
+        return null;
       },
     },
   });
